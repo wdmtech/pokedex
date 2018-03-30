855dbf7475346ad7a6697ba379633466e568c1c2
Will Murray <willdavidmurray@gmail.com>
2018-03-30 16:07:58 +0200

    :sparkles: Add types service

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,5 +1,7 @@
 const pokemon = require('./pokemon/pokemon.service.js');
+const types = require('./types/types.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(pokemon);
+  app.configure(types);
 };
