2912b871d735b43da1ac083186d656dd890e78f1
Will Murray <willdavidmurray@gmail.com>
2018-03-30 16:06:24 +0200

    :sparkles: add more types

--- server/src/seeds/index.js
+++ server/src/seeds/index.js
@@ -4,9 +4,16 @@ module.exports = async function(app) {
     FIRE: 'fire',
     FLYING: 'flying',
     WATER: 'water',
+    FIGHTING: 'fighting',
+    ELECTRICAL: 'electrical',
     PLANT: 'plant',
     POISON: 'poison',
     ICE: 'ice',
+    FAIRY: 'fairy',
+    BUG: 'bug',
+    GHOST: 'ghost',
+    ROCK: 'rock',
+    GROUND: 'ground',
     PSYCHIC: 'psychic',
   }
 
@@ -16,12 +23,14 @@ module.exports = async function(app) {
       description: 'A fiery, dragon type Pokémon, that will roast you if you get too close.',
       image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
       types: [TYPES.FIRE, TYPES.FLYING],
+      weaknesses: [TYPES.WATER],
     },
     {
       name: 'Bulbasaur',
       description: 'It is easy to see Bulbasaur taking a nap in the sun. The seed that it has in the back is growing more and more as it absorbs the rays of the sun.',
       image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
       types: [TYPES.PLANT, TYPES.POISON],
+      weaknesses: [TYPES.FIRE],
     }
   ]
 
