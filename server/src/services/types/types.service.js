79519786354e403a274b54d64446dd17ef6e68a6
Will Murray <willdavidmurray@gmail.com>
2018-03-30 16:18:19 +0200

    :fire: Remove pagination

--- server/src/services/types/types.service.js
+++ server/src/services/types/types.service.js
@@ -5,12 +5,10 @@ const hooks = require('./types.hooks');
 
 module.exports = function (app) {
   const Model = createModel(app);
-  const paginate = app.get('paginate');
 
   const options = {
     name: 'types',
     Model,
-    paginate
   };
 
   // Initialize our service with any options it requires
