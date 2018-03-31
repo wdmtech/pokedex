7a667f83ce76cc4a4bb15b1f478064236115895e
Will Murray <willdavidmurray@gmail.com>
2018-03-31 18:26:20 +0100

    :fire: Remove pagination

--- server/src/services/pokemon/pokemon.service.js
+++ server/src/services/pokemon/pokemon.service.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
   const options = {
     name: 'pokemon',
     Model,
-    paginate
+    // paginate
   };
 
   // Initialize our service with any options it requires
