1bc5e7ec2e83cb12128af0c41d540a46fb0ee2bc
Will Murray <willdavidmurray@gmail.com>
2018-03-31 18:28:07 +0100

    :pencil2: Rewrite seeder to include all new attributes in models

--- server/src/seeds/index.js
+++ server/src/seeds/index.js
@@ -14,7 +14,7 @@ module.exports = async function(app) {
       { name: 'flying', color: 'blue' },
       { name: 'water', color: 'lightblue' },
       { name: 'fighting', color: 'orange' },
-      { name: 'electrical', color: 'darkyellow' },
+      { name: 'electric', color: '#afaf0c' },
       { name: 'plant', color: 'green' },
       { name: 'poison', color: 'darkgreen' },
       { name: 'ice', color: 'deepblue' },
@@ -46,6 +46,32 @@ module.exports = async function(app) {
     let result = await pokemonService.create(
       [
         {
+          name: 'Charmander',
+          description: 'The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.',
+          image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+          types: [
+            await getOrCreateTypeByName(typesData, 'fire'),
+          ],
+          weaknesses: [
+            await getOrCreateTypeByName(typesData, 'ground'),
+            await getOrCreateTypeByName(typesData, 'rock'),
+            await getOrCreateTypeByName(typesData, 'water'),
+          ],
+        },
+        {
+          name: 'Charmelion',
+          description: 'Charmeleon mercilessly destroys its foes using its sharp claws. If it encounters a strong foe, it turns aggressive. In this excited state, the flame at the tip of its tail flares with a bluish white color.',
+          image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
+          types: [
+            await getOrCreateTypeByName(typesData, 'fire'),
+          ],
+          weaknesses: [
+            await getOrCreateTypeByName(typesData, 'ground'),
+            await getOrCreateTypeByName(typesData, 'rock'),
+            await getOrCreateTypeByName(typesData, 'water'),
+          ],
+        },
+        {
           name: 'Charizard',
           description: 'A fiery, dragon type Pokémon, that will roast you if you get too close.',
           image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
@@ -54,6 +80,8 @@ module.exports = async function(app) {
             await getOrCreateTypeByName(typesData, 'flying'),
           ],
           weaknesses: [
+            await getOrCreateTypeByName(typesData, 'rock'),
+            await getOrCreateTypeByName(typesData, 'electric'),
             await getOrCreateTypeByName(typesData, 'water'),
           ],
         },
@@ -73,6 +101,38 @@ module.exports = async function(app) {
     )
 
     console.log(result)
+
+    const setupEvolutions = async (name = null, evolvesFromName = null, evolvesToName = null) => {
+      // Get the Pokémon to modify
+      let pokemon = await pokemonService.find({ query: { name }, $limit: 1 })
+
+      if (pokemon && pokemon.length) {
+        pokemon = pokemon[0]
+
+        if (evolvesFromName && evolvesFromName.length) {
+          let pokemonEvolvesFrom = await pokemonService.find({ query: { name: evolvesFromName } })
+          if (pokemonEvolvesFrom && pokemonEvolvesFrom.length) {
+            pokemonEvolvesFrom = pokemonEvolvesFrom[0]
+            await pokemonService.patch(pokemon._id, { evolves_from: pokemonEvolvesFrom._id })
+          }
+        }
+
+        if (evolvesToName && evolvesToName.length) {
+          let pokemonEvolvesTo = await pokemonService.find({ query: { name: evolvesToName } })
+          if (pokemonEvolvesTo && pokemonEvolvesTo.length) {
+            pokemonEvolvesTo = pokemonEvolvesTo[0]
+            await pokemonService.patch(pokemon._id, { evolves_to: pokemonEvolvesTo._id })
+          }
+        }
+
+      } else {
+        console.error(`Pokémon '${name}'does not exist! Couldn't set up evolutions.`)
+      }
+    }
+
+    await setupEvolutions('Charmander', null, 'Charmelion');
+    await setupEvolutions('Charmelion', 'Charmander', 'Charizard');
+    await setupEvolutions('Charizard', 'Charmelion', null);
 
   } catch (e) {
 
