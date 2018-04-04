adeba25a65152ca9ddd8a5c64b12f9605ab5e975
Will Murray <willdavidmurray@gmail.com>
2018-04-04 17:22:18 +0100

    :rocket: Add Docker deployment configs

--- server/src/mongoose.js
+++ server/src/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
-  mongoose.connect(app.get('mongodb'), {});
+  mongoose.connect(process.env.DATABASE_URL || app.get('mongodb'), {});
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
