4b6676b566dae143ccdab0d27dae3377d4bd9146
Will Murray <willdavidmurray@gmail.com>
2018-04-05 18:39:45 +0100

    :memo: load types service first

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,7 +1,7 @@
-const pokemon = require('./pokemon/pokemon.service.js');
 const types = require('./types/types.service.js');
+const pokemon = require('./pokemon/pokemon.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
-  app.configure(pokemon);
   app.configure(types);
+  app.configure(pokemon);
 };
